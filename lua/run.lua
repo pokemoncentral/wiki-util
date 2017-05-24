82ff73705d8ea9d8ae23a1f70dd5d4313e22cf97
Davide Laezza <truzzialrogo@gmx.com>
2017-05-25 01:44:26 +0200

    Commented run.lua

--- lua/run.lua
+++ lua/run.lua
@@ -1,10 +1,27 @@
-local modulesPath = arg[2] or '../modules'
+--[[
+
+This script is meant as an entry point
+for all the other ones: it adds the
+path to Scribunto-equivalent modules to
+package.path and then runs the provided
+script.
+
+--]]
+
+--[[
+
+Since it rarely changes, it's less
+convenient to have a command line
+parameter for this.
+
+--]]
+local MODULES_PATH = ADD_YOUR_PATH_HERE
 
 package.path = table.concat{
 	package.path,
 	';',
-	modulesPath,
+	MODULES_PATH,
 	'/?.lua'
 }
 
-require((arg[1]:gsub('%.lua', '')))
+require((table.remove(arg, 1):gsub('%.lua', '')))
