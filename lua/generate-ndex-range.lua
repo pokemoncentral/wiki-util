37e80f3a59424e6e5fb5c47e2021678341b02c3f
Davide Laezza <truzzialrogo@gmx.com>
2017-05-26 10:45:36 +0200

    Ndex generation lua script now works properly

--- lua/generate-ndex-range.lua
+++ lua/generate-ndex-range.lua
@@ -4,14 +4,24 @@ local pokes = require('Poké-data')
 local useless = require('UselessForms-data')
 
 local range = table.keys(pokes)
-for ndex, data in ipairs(useless) do
-	for abbr in pairs(data.names) do
-		table.insert(range, string.tf(ndex) .. abbr)
+range = table.filter(range, string.parseInt)
+range = table.map(range, function(ndex)
+	return type(ndex) == 'string'
+			and ndex
+			or string.tf(ndex)
+end)
+	
+for ndex, data in pairs(useless) do
+	if tonumber(ndex) then
+		for abbr in pairs(data.names) do
+			abbr = abbr == 'base' and '' or abbr
+			table.insert(range, string.tf(ndex) .. abbr)
+		end
 	end
 end
 
 io.output(arg[1])
 
-for poke in ipairs(range) do
+for _, poke in ipairs(range) do
 	io.write(table.concat{arg[2], poke, arg[3], '\n'})
 end
