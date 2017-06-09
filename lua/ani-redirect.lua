16ce098d532d1ff64deb54f716abf0dcda71bcca
Davide Laezza <truzzialrogo@gmx.com>
2017-06-09 18:21:19 +0200

    Done with lua scripts as well

---
+++ lua/ani-redirect.lua
@@ -0,0 +1,58 @@
+--[[
+
+This script creates redirects file for
+mini-sprite of the current generation
+from animated to static.
+
+Arguments:
+	- $1: specifies which redirect are
+		to eb created. Can be one
+		of these:
+		- 'all': Creates redirects for
+			all Pokémon.
+		- <gen number>: Creates redirects
+			for all Pokémon introduced in
+			in the first game of the given
+			generation.
+		- <game>: Creates redirects for
+			all Pokémon and alternate
+			forms introduced in the passed
+			game.
+	- $2: output filename
+
+--]]
+
+local str = require('Wikilib-strings')
+local makeDictPage = require('dict-page')
+local ndexes = require('ndexes')
+
+local source
+if arg[1] == 'all' then
+	source = ndexes.all
+	
+elseif tonumber(arg[1]) then
+	source = ndexes.gen(arg[1])
+	
+else
+	source = ndexes.game(arg[1])
+	
+end
+
+io.output(arg[2])
+
+for _, ndex in pairs(source) do
+	local title = string.interp(
+		'File:Ani${ndex}MS.gif',
+		{
+			ndex = ndex
+		}
+	)
+	local content = string.interp(
+		'#RINVIA [[File:${ndex}MS.png]]',
+		{
+			ndex = ndex
+		}
+	)
+
+	io.write(makeDictPage(title, content))
+end
