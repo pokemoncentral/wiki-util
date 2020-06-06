bff7f8f5f5637aa892414647ec4aa8d74a0a92cb
Davide <truzzialrogo@gmx.com>
2020-06-06 19:16:49 +0200

    Adding code to run.sh so that scripts can be safely run from everywhere on the filesystem (#1)

--- bash/run.sh
+++ bash/run.sh
@@ -1,9 +1,33 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-. ./config.sh
+#######################################
+# Initial setup
+#######################################
 
-SCRIPT="$1"
+PARENT_DIR="$(dirname "${BASH_SOURCE[0]}" | xargs readlink -f)"
+source "$PARENT_DIR/config.sh"
 
+#######################################
+# Input processing
+#######################################
+
+SCRIPT="$PARENT_DIR/$(basename "$1")"
 shift 1
 
+[ ! -f "$SCRIPT" ] && {
+    echo >&2 "Script not found: $1"
+    exit 1
+}
+
+#######################################
+# Invoking script
+#######################################
+
+# Cd-ing into parent directory so that the executed scripts can assume to be
+# running there
+cd "$PARENT_DIR" || exit
+
 bash "$SCRIPT" "$@"
+
+# Cd-ing back so as not to pollute the caller's environment
+cd - &> /dev/null || exit
