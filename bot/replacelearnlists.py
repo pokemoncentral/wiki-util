f8be772166953ab6f03dcf4fca52b973e4bb1b4d
Flavio <flavio.a.618@gmail.com>
2022-11-04 17:34:34 +0100

    [PokeMoves-AutoGen] Updating bot script, adding pre-commit hooks

--- bot/replacelearnlists.py
+++ bot/replacelearnlists.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 r"""
-This bot replaces learnlists with the output of a command.
-Just call it with a generator that yields Pokémon's pages, and it does the
-rest on its own.
+This bot replaces learnlists with the output of a lua script.
 
-Pls don't bother me with "everything is hardcoded bad practice blablabla"
-because right now I don't have time to do something nice, and I know in
-the future I won't change it.
-Moreover the command is meant to be run once in a while and the hardcoded part
-doesn't change.
+The first two positional arguments are, respectively, the lua script to run
+(that is expected to be `wiki-util/pokemoves-autogen/get-learnlist.lua`) and
+the generation.
+
+The bot works on pages specified by a generator, such as the category of
+learnset subpages.
 
 &pagegenerator-params;
 """
@@ -25,19 +24,22 @@ import mwparserfromhell as mwparser
 import pywikibot as pwb
 from pywikibot import pagegenerators
 from pywikibot.bot import (
-    ExistingPageBot, FollowRedirectPageBot, MultipleSitesBot,
-    QuitKeyboardInterrupt
+    ExistingPageBot,
+    FollowRedirectPageBot,
+    MultipleSitesBot,
+    QuitKeyboardInterrupt,
 )
 
 # This is required for the text that is shown when you run this script with the
 # parameter -help.
-docuReplacements = {'&pagegenerator-params;': pagegenerators.parameterHelp}
+docuReplacements = {"&pagegenerator-params;": pagegenerators.parameterHelp}
 
 # Constant with gen name and number
 GENERATIONS = {
-    8: { "text": "ottava", "roman": "VIII", "number": 8 },
-    9: { "text": "nona", "roman": "IX", "number": 9 },
+    8: {"text": "ottava", "roman": "VIII", "number": 8},
+    9: {"text": "nona", "roman": "IX", "number": 9},
 }
+
 
 class SectionsBot(MultipleSitesBot, ExistingPageBot, FollowRedirectPageBot):
     # Constants specifying which sections to treat and parameters for the command
@@ -48,7 +50,7 @@ class SectionsBot(MultipleSitesBot, ExistingPageBot, FollowRedirectPageBot):
         ("Tramite \[\[MT\]\]/\[\[MN\]\]", "tm"),
         ("Tramite \[\[Accoppiamento Pokémon\|accoppiamento\]\]", "breed"),
         ("Dall'\[\[Insegnamosse\]\]", "tutor"),
-        ("Tramite \[\[evoluzione\|evoluzioni\]\] precedenti", "preevo")
+        ("Tramite \[\[evoluzione\|evoluzioni\]\] precedenti", "preevo"),
     ]
 
     BASE_NOINCLUDE_CATS = """<noinclude>
@@ -64,45 +66,55 @@ class SectionsBot(MultipleSitesBot, ExistingPageBot, FollowRedirectPageBot):
     @keyword always: Whether to prompt the user before uploading changes.
     @type always: bool
     """
+
     def __init__(self, summary: str, command_path: str, generation: int, **kwargs):
         """Initializer."""
         super(SectionsBot, self).__init__(**kwargs)
 
         self.command_path = command_path
-        self.summary = (summary or 'Bot: replacing learnlists')
+        self.summary = summary or "Bot: replacing learnlists"
         self.generation = GENERATIONS[int(generation)]
-
 
     def get_noinclude_cats(self):
         """Get the final noinclude."""
         return SectionsBot.NOINCLUDE_CATS.format(
-            gen_text=self.generation["text"],
-            gen_roman=self.generation["roman"]
+            gen_text=self.generation["text"], gen_roman=self.generation["roman"]
         )
 
-    def treat(self, page):
+    def treat_page(self):
         """Replaces the content of each section in the list with the output
         of a certain command, whose arguments are the page title and
         anything determined by the section
         """
         # Get roottitle
-        title = page.title()
+        title = self.current_page.title()
         rootidx = title.find("/")
         roottitle = title if rootidx == -1 else title[:rootidx]
 
         # Parsing the page
-        content = mwparser.parse(page.text, skip_style_tags=True)
+        content = mwparser.parse(self.current_page.text, skip_style_tags=True)
 
         # Getting the final noinclude
         try:
-            noinclude_cats = content.filter_tags(recursive=False, matches=lambda c: str(c.tag) == "noinclude")[0]
+            noinclude_cats = content.filter_tags(
+                recursive=False, matches=lambda c: str(c.tag) == "noinclude"
+            )[0]
             links = noinclude_cats.contents.filter_wikilinks(recursive=False)
-            cat = list(filter(lambda c: str(c.title).startswith(("Categoria:", "Category:")), links))[0]
+            cat = list(
+                filter(
+                    lambda c: str(c.title).startswith(("Categoria:", "Category:")),
+                    links,
+                )
+            )[0]
             content.remove(noinclude_cats)
         except IndexError:
             # Here something went wrong, hence rebuild the final noinclude
-            pwb.warning("Problems finding the final noinclude, rebuilding from scratches")
-            noinclude_cats = mwparser.parse(self.get_noinclude_cats().format(poke=roottitle))
+            pwb.warning(
+                "Problems finding the final noinclude, rebuilding from scratches"
+            )
+            noinclude_cats = mwparser.parse(
+                self.get_noinclude_cats().format(poke=roottitle)
+            )
             pwb.output(noinclude_cats)
             rebuild = pwb.input_yn("Is this correct?", default=True)
             if not rebuild:
@@ -116,15 +128,26 @@ class SectionsBot(MultipleSitesBot, ExistingPageBot, FollowRedirectPageBot):
         for heading, kind in SectionsBot.SECTIONS:
             for section in limited_content.get_sections(matches=heading):
                 real_heading = section.filter_headings()[0]
-                newcontent = subprocess.run(["lua", self.command_path, roottitle, kind, str(self.generation["number"])], capture_output=True, encoding="utf-8")
-                content.replace(section, str(real_heading) + "\n" + newcontent.stdout + "\n")
+                newcontent = subprocess.run(
+                    [
+                        "lua",
+                        self.command_path,
+                        roottitle,
+                        kind,
+                        str(self.generation["number"]),
+                    ],
+                    capture_output=True,
+                    encoding="utf-8",
+                )
+                content.replace(
+                    section, str(real_heading) + "\n" + newcontent.stdout + "\n"
+                )
 
         # Adding category and interwiki
         content.append(noinclude_cats)
 
         # Uploading the page if necessary
-        self.userPut(page=page, oldtext=page.text, newtext=str(content),
-                     summary=self.summary)
+        self.put_current(str(content), summary=self.summary)
 
 
 def main(*args):
@@ -154,15 +177,15 @@ def main(*args):
 
         # By convention, all the arguments not starting with - are considered
         # positional.
-        if not arg.startswith('-'):
+        if not arg.startswith("-"):
             pos_args.append(arg)
             continue
 
-        arg_name, _, arg_value = arg[1:].partition(':')
+        arg_name, _, arg_value = arg[1:].partition(":")
 
-        if arg_name == 'always':
+        if arg_name == "always":
             always = True
-        elif arg_name == 'summary':
+        elif arg_name == "summary":
             summary = arg_value
 
     bot = SectionsBot(
@@ -174,5 +197,6 @@ def main(*args):
     )
     bot.run()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
