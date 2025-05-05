6613e02d1ccfa2be2a2e298ec7139599674d6649
Cruifer <38588039+Cruifer@users.noreply.github.com>
2025-05-05 10:32:26 +0200

    fix nome variabile

--- gccp/gccp-translate-dir.py
+++ gccp/gccp-translate-dir.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
 
         basename = os.path.basename(fullname)
         pagename = str(basename).rsplit(".", 1)[0]
-        outname = os.path.join(OUTPUT_DIR, str(basename).replace("TCG", "GCC"))
+        outname = os.path.join(output_dir, str(basename).replace("TCG", "GCC"))
         with open(outname, "w", encoding="utf-8") as out_stream:
             print(gccptranslatepage.translate_page(source, pagename), file=out_stream)
             print(f"Translated file {basename} ({pagename})")
